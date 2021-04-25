70d80590f06ba000d0dc3dcc6b3b34ecb726be00
Luligabi1 <luligabi111@gmail.com>
2021-04-25 20:41:44 -0300

    Added proper ItemGroup.

--- src/main/java/me/luligabi/vanillaaiots/VanillaAiots.java
+++ src/main/java/me/luligabi/vanillaaiots/VanillaAiots.java
@@ -3,6 +3,10 @@ package me.luligabi.vanillaaiots;
 import me.luligabi.vanillaaiots.registry.EffectiveBlocksRegistry;
 import me.luligabi.vanillaaiots.registry.ToolRegistry;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 
 public class VanillaAiots implements ModInitializer {
 
@@ -13,4 +17,9 @@ public class VanillaAiots implements ModInitializer {
         EffectiveBlocksRegistry.register();
         ToolRegistry.register();
     }
+
+    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.create(
+            new Identifier(MOD_ID, "item_group"))
+            .icon(() -> new ItemStack(ToolRegistry.DIAMOND_AIOT))
+            .build();
 }
