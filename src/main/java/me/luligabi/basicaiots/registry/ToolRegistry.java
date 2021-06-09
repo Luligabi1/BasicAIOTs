13f15a80e977fe3fa790de07d5b77490cc1dd786
Luligabi1 <luligabi111@gmail.com>
2021-06-09 12:31:44 -0300

    Reverting commit be763eda

--- src/main/java/me/luligabi/basicaiots/registry/ToolRegistry.java
+++ src/main/java/me/luligabi/basicaiots/registry/ToolRegistry.java
@@ -15,7 +15,7 @@ import net.minecraft.util.registry.Registry;
 
 public class ToolRegistry {
 
-    public static void register() {
+    public static void init() {
         Registry.register(Registry.ITEM, new Identifier(BasicAiots.MOD_ID, "wooden_aiot"), WOODEN_AIOT);
         Registry.register(Registry.ITEM, new Identifier(BasicAiots.MOD_ID, "stone_aiot"), STONE_AIOT);
         Registry.register(Registry.ITEM, new Identifier(BasicAiots.MOD_ID, "golden_aiot"), GOLDEN_AIOT);
@@ -28,7 +28,6 @@ public class ToolRegistry {
     public static void indrevRegister() {
         Registry.register(Registry.ITEM, new Identifier(BasicAiots.MOD_ID, "tin_aiot"), TIN_AIOT);
         Registry.register(Registry.ITEM, new Identifier(BasicAiots.MOD_ID, "copper_aiot"), COPPER_AIOT);
-        Registry.register(Registry.ITEM, new Identifier(BasicAiots.MOD_ID, "silver_aiot"), SILVER_AIOT);
         Registry.register(Registry.ITEM, new Identifier(BasicAiots.MOD_ID, "steel_aiot"), STEEL_AIOT);
         Registry.register(Registry.ITEM, new Identifier(BasicAiots.MOD_ID, "lead_aiot"), LEAD_AIOT);
     }
@@ -41,36 +40,37 @@ public class ToolRegistry {
     }
 
     // Vanilla
-    public static ToolItem WOODEN_AIOT = new AiotToolItem(3, -2.4F, ToolMaterials.WOOD, new Item.Settings().group(BasicAiots.ITEM_GROUP));
+    public static ToolItem WOODEN_AIOT = new AiotToolItem(3, -2.4F, ToolMaterials.WOOD, EffectiveBlocksRegistry.effectiveBlocks, new Item.Settings().group(BasicAiots.ITEM_GROUP));
 
-    public static ToolItem STONE_AIOT = new AiotToolItem(3, -2.4F, ToolMaterials.STONE, new Item.Settings().group(BasicAiots.ITEM_GROUP));
+    public static ToolItem STONE_AIOT = new AiotToolItem(3, -2.4F, ToolMaterials.STONE, EffectiveBlocksRegistry.effectiveBlocks, new Item.Settings().group(BasicAiots.ITEM_GROUP));
 
-    public static ToolItem GOLDEN_AIOT = new AiotToolItem(3, -2.4F, ToolMaterials.GOLD, new Item.Settings().group(BasicAiots.ITEM_GROUP));
+    public static ToolItem GOLDEN_AIOT = new AiotToolItem(3, -2.4F, ToolMaterials.GOLD, EffectiveBlocksRegistry.effectiveBlocks, new Item.Settings().group(BasicAiots.ITEM_GROUP));
 
-    public static ToolItem IRON_AIOT = new AiotToolItem(3, -2.4F, ToolMaterials.IRON, new Item.Settings().group(BasicAiots.ITEM_GROUP));
+    public static ToolItem IRON_AIOT = new AiotToolItem(3, -2.4F, ToolMaterials.IRON, EffectiveBlocksRegistry.effectiveBlocks, new Item.Settings().group(BasicAiots.ITEM_GROUP));
 
-    public static ToolItem DIAMOND_AIOT = new AiotToolItem(3, -2.4F, ToolMaterials.DIAMOND, new Item.Settings().group(BasicAiots.ITEM_GROUP));
+    public static ToolItem DIAMOND_AIOT = new AiotToolItem(3, -2.4F, ToolMaterials.DIAMOND, EffectiveBlocksRegistry.effectiveBlocks, new Item.Settings().group(BasicAiots.ITEM_GROUP));
 
-    public static ToolItem NETHERITE_AIOT = new AiotToolItem(3, -2.4F, ToolMaterials.NETHERITE, new Item.Settings().group(BasicAiots.ITEM_GROUP).fireproof());
+    public static ToolItem NETHERITE_AIOT = new AiotToolItem(3, -2.4F, ToolMaterials.NETHERITE, EffectiveBlocksRegistry.effectiveBlocks, new Item.Settings().group(BasicAiots.ITEM_GROUP));
 
     // Industrial Revolution
-    public static ToolItem TIN_AIOT = new AiotToolItem(3, -2.4F, TinToolMaterial.INSTANCE, new Item.Settings().group(BasicAiots.ITEM_GROUP));
+    public static ToolItem TIN_AIOT = new AiotToolItem(3, -2.4F, TinToolMaterial.INSTANCE, EffectiveBlocksRegistry.effectiveBlocks, new Item.Settings().group(BasicAiots.ITEM_GROUP));
 
-    public static ToolItem COPPER_AIOT = new AiotToolItem(3, -2.4F, CopperToolMaterial.INSTANCE, new Item.Settings().group(BasicAiots.ITEM_GROUP));
+    public static ToolItem COPPER_AIOT = new AiotToolItem(3, -2.4F, CopperToolMaterial.INSTANCE, EffectiveBlocksRegistry.effectiveBlocks, new Item.Settings().group(BasicAiots.ITEM_GROUP));
 
-    public static ToolItem SILVER_AIOT = new AiotToolItem(3, -2.4F, SilverToolMaterial.INSTANCE, new Item.Settings().group(BasicAiots.ITEM_GROUP));
+    public static ToolItem SILVER_AIOT = new AiotToolItem(3, -2.4F, SilverToolMaterial.INSTANCE, EffectiveBlocksRegistry.effectiveBlocks, new Item.Settings().group(BasicAiots.ITEM_GROUP));
 
-    public static ToolItem STEEL_AIOT = new AiotToolItem(3, -2.4F, SteelToolMaterial.INSTANCE, new Item.Settings().group(BasicAiots.ITEM_GROUP));
+    public static ToolItem STEEL_AIOT = new AiotToolItem(3, -2.4F, SteelToolMaterial.INSTANCE, EffectiveBlocksRegistry.effectiveBlocks, new Item.Settings().group(BasicAiots.ITEM_GROUP));
 
-    public static ToolItem LEAD_AIOT = new AiotToolItem(3, -2.4F, LeadToolMaterial.INSTANCE, new Item.Settings().group(BasicAiots.ITEM_GROUP));
+    public static ToolItem LEAD_AIOT = new AiotToolItem(3, -2.4F, LeadToolMaterial.INSTANCE, EffectiveBlocksRegistry.effectiveBlocks, new Item.Settings().group(BasicAiots.ITEM_GROUP));
 
     // Tech Reborn
-    public static ToolItem RUBY_AIOT = new AiotToolItem(3, -2.4F, RubyToolMaterial.INSTANCE, new Item.Settings().group(BasicAiots.ITEM_GROUP));
+    public static ToolItem RUBY_AIOT = new AiotToolItem(3, -2.4F, RubyToolMaterial.INSTANCE, EffectiveBlocksRegistry.effectiveBlocks, new Item.Settings().group(BasicAiots.ITEM_GROUP));
 
-    public static ToolItem SAPPHIRE_AIOT = new AiotToolItem(3, -2.4F, SapphireToolMaterial.INSTANCE, new Item.Settings().group(BasicAiots.ITEM_GROUP));
+    public static ToolItem SAPPHIRE_AIOT = new AiotToolItem(3, -2.4F, SapphireToolMaterial.INSTANCE, EffectiveBlocksRegistry.effectiveBlocks, new Item.Settings().group(BasicAiots.ITEM_GROUP));
 
-    public static ToolItem PERIDOT_AIOT = new AiotToolItem(3, -2.4F, PeridotToolMaterial.INSTANCE, new Item.Settings().group(BasicAiots.ITEM_GROUP));
+    public static ToolItem PERIDOT_AIOT = new AiotToolItem(3, -2.4F, PeridotToolMaterial.INSTANCE, EffectiveBlocksRegistry.effectiveBlocks, new Item.Settings().group(BasicAiots.ITEM_GROUP));
 
     // Other
-    public static ToolItem BRONZE_AIOT = new AiotToolItem(3, -2.4F, BronzeToolMaterial.INSTANCE, new Item.Settings().group(BasicAiots.ITEM_GROUP));
+    public static ToolItem BRONZE_AIOT = new AiotToolItem(3, -2.4F, BronzeToolMaterial.INSTANCE, EffectiveBlocksRegistry.effectiveBlocks, new Item.Settings().group(BasicAiots.ITEM_GROUP));
+
 }
