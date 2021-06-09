3a65da9dec1aefdb97b253fe881fd5bb66da716f
Luligabi1 <luligabi111@gmail.com>
2021-06-09 15:12:24 -0300

    Compatibility AIOTs will now load, regardless of if it has the original mod. Addition tooltips.

---
+++ src/main/java/me/luligabi/basicaiots/item/IndustrialRevolutionTechRebornToolItem.java
@@ -0,0 +1,24 @@
+package me.luligabi.basicaiots.item;
+
+import me.luligabi.basicaiots.AiotToolItem;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ToolMaterial;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.util.Formatting;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+public class IndustrialRevolutionTechRebornToolItem extends AiotToolItem {
+
+    public IndustrialRevolutionTechRebornToolItem(float attackDamage, float attackSpeed, ToolMaterial material, Settings settings) {
+        super(attackDamage, attackSpeed, material, settings);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        tooltip.add(new TranslatableText("item.basicaiots.indrev_techreborn.tooltip").formatted(Formatting.BLUE, Formatting.ITALIC, Formatting.BOLD));
+    }
+}
